9adc5c4d38d975606f4cd6444ddcc5581a24dada
Ubuntu <ubuntu@ip-172-31-36-76.us-east-2.compute.internal>
2022-11-18 16:09:47 +0000

    initial commit

---
+++ api/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+import json
+import mysql.connector
+
+app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      passwd="root",
+      database="campaign"
+)
+
+@app.route("/voucher", methods=['POST'])
+def get_voucher():
+    try:
+        data = request.get_json()
+        print(data)
+        segment_name = data["segment_name"]
+
+        print(f"Fetching voucher for segment_name: {segment_name}")
+
+        total_orders = data["total_orders"]
+        cursor = mydb.cursor()
+        sql = "select voucher_amount from voucher_log where total_orders >=14 AND total_orders <= 37 order by count desc limit 1";
+        cursor.execute(sql)
+
+        result = {}
+        for row in cursor.fetchall():
+            result["voucher_amount"] = row[0]
+
+        return result
+
+    except BaseException as e:
+        return (f"Exception: {e}")
+
+    
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ mysql/create.sql
@@ -0,0 +1,10 @@
+create database campaign;
+
+CREATE TABLE `campaign.voucher_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `country` varchar(100) NOT NULL,
+  `total_orders` int(11) NOT NULL,
+  `voucher_amount` float NOT NULL, 
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
